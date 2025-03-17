4b7a78704b0ff0547c3cbb23309552e430776dab
Ken Lin <kenlinahs@gmail.com>
2025-03-16 22:47:47 -0400

    responsive sticky nav bar

--- Personal Site/src/App.jsx
+++ Personal Site/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavBar from './components/NavBar'
 import NavButton from './components/NavButton'
 import LinkButton from './components/LinkButton'
 import emailIcon from './assets/envelope-solid.svg'
@@ -8,10 +7,18 @@ import linkedInIcon from './assets/linkedin-brands.svg'
 
 const App = () => {
   return (
-    <>
-      <NavBar>
-        <p className="block text-8xl text-white">Ken Lin</p>
-        <nav id="nav-items">
+    <div className="flex flex-row space-x-5">
+      <div
+        id="nav-bar"
+        className="sticky z-10 flex h-screen w-1/6 flex-col items-center justify-between space-y-10 sm:w-3/12"
+      >
+        <p className="block text-2xl text-nowrap text-white sm:text-4xl md:text-5xl lg:text-7xl xl:text-8xl">
+          Ken Lin
+        </p>
+        <nav
+          id="nav-items"
+          className="flex h-1/3 w-full flex-col content-center space-y-7 pr-10 pl-5 text-xs sm:h-1/3 sm:space-y-10 sm:pl-10 sm:text-sm md:pl-15 md:text-base lg:pl-25 lg:text-xl xl:pl-40"
+        >
           <a className="block">About Me</a>
           <a className="block">Projects</a>
           <a className="block">Work Experience</a>
@@ -19,7 +26,7 @@ const App = () => {
         </nav>
         <nav
           id="links-container"
-          className="flex h-20 w-full flex-row justify-between"
+          className="flex flex-shrink-0 flex-col justify-between space-y-2 space-x-3 sm:h-7 sm:w-25 sm:flex-row md:h-15 md:w-50 lg:h-20 lg:w-70"
         >
           <LinkButton
             href={'mailto:kenlinahs@gmail.com'}
@@ -35,13 +42,26 @@ const App = () => {
           />
           <LinkButton
             href={'https://www.linkedin.com/in/ken-j-lin'}
-            icon={emailIcon}
+            icon={linkedInIcon}
             id={'email-icon'}
             alt={'Email Icon'}
           />
         </nav>
-      </NavBar>
-    </>
+      </div>
+      <div id="section-cont" className="flex items-center">
+        <section id="about-me">
+          <div>
+            <h1>A Little About Me!</h1>
+            <p>
+              My name is Ken Lin, I am currently a senior at Stony Brook
+              University majoring in Computer Engineering. I am an aspiring
+              Full-Stack developer who loves to learn new technologies and
+              leverage them to solve real-world problems.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
 
--- Personal Site/src/components/LinkButton.jsx
+++ Personal Site/src/components/LinkButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const LinkButton = ({ icon, id, alt, href }) => {
+const LinkButton = ({ icon, href, ...props }) => {
   return (
     <a href={href} target="_blank">
-      <img src={icon} id={id} alt={alt} className="z-20 h-14 w-14" />
+      <img src={icon} {...props} className="z-20 h-8 w-8 md:h-14 md:w-14" />
     </a>
   )
 }
