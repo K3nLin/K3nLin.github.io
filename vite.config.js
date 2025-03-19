48f1d9838b521356810661128806d56e6e128792
Ken Lin <kenlinahs@gmail.com>
2025-03-19 15:19:17 -0400

    testing

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/K3nLin.github.io',
+  base: '/K3nLin.github.io/',
 })
